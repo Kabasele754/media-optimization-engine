f2290cdff94a31919c48dc8215bf7a967ed7b61a
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2026-09-25 15:38:34 +0200

    Add media_engine/processors/panorama/__init__.py

---
+++ media_engine/processors/panorama/__init__.py
@@ -0,0 +1,5 @@
+from .equirectangular import Panorama360Processor
+from .manifest import panorama_manifest
+from .detect import detect_equirectangular
+
+__all__ = ['Panorama360Processor', 'panorama_manifest', 'detect_equirectangular']
