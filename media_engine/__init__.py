6b8c8a3b35c533cc569ef3046c41de22ee20f73b
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2026-09-25 15:30:46 +0200

    Add media_engine/__init__.py

---
+++ media_engine/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.4.0'
+
+def register_model_image(*args, **kwargs):
+    from .registry import register_model_image as _register
+    return _register(*args, **kwargs)
+
+default_app_config = 'media_engine.apps.MediaEngineConfig'
